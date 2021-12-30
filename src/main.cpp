d4808b0ca1b8db9deab5062bb103003e10360c99
Ben Mann <ben@benmann.net>
2021-12-30 16:28:12 +0800

    Some minor inlining, and 5.71 to 7.57fps improvement

--- src/main.cpp
+++ src/main.cpp
@@ -254,6 +254,18 @@ void blitCharacterVSlice(char packedCharacter, char xSlice, char bufferOffset) {
 //               56          60          64                70    72
 char target[] = { 0, 0, 0, 0, 0, 0, 1, 2, 3, 2, 1, 0, 0, 0, 0, 0, 0 };
 
+#define AVR_WRITESPI(x) for (SPDR = (x); (!(SPSR & _BV(SPIF)));)
+
+// FPS was 5.68-5.71. After adding this, it's 7.57
+void _writePixels(uint16_t *colors, uint32_t len) {
+    while (len--) {
+//        SPI_WRITE16(*colors++);
+        uint16_t w = *colors++;
+        AVR_WRITESPI(w >> 8);
+        AVR_WRITESPI(w);
+    }
+}
+
 // This is relatively fast.
 void drawHorizonVWritePixels(int16_t x, int16_t yintercept) {
     bool hasCharsX = x < 6 * 5;
@@ -292,7 +304,8 @@ void drawHorizonVWritePixels(int16_t x, int16_t yintercept) {
             colourBuffer[target[x - 56]] = YELLOW;
         }
 
-        tft.writePixels(colourBuffer, 16, true, false);
+        // tft.writePixels(colourBuffer, 16, true, false);
+        _writePixels(colourBuffer, 16);
     }
 }
 
