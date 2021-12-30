5031b7badb1a934179b67bfcfdc795497061da5f
Ben Mann <ben@benmann.net>
2021-12-30 13:52:51 +0800

    Various experiments to get a horizon rendering approach which might be fast enough for what we want

--- src/main.cpp
+++ src/main.cpp
@@ -67,197 +67,17 @@ char hasMpu = 0;
 
 float p = 3.1415926;
 
-
-
-
-void testlines(uint16_t color) {
-    tft.fillScreen(BLACK);
-    for (uint16_t x=0; x < tft.width()-1; x+=6) {
-        tft.drawLine(0, 0, x, tft.height()-1, color);
-    }
-    for (uint16_t y=0; y < tft.height()-1; y+=6) {
-        tft.drawLine(0, 0, tft.width()-1, y, color);
-    }
-
-    tft.fillScreen(BLACK);
-    for (uint16_t x=0; x < tft.width()-1; x+=6) {
-        tft.drawLine(tft.width()-1, 0, x, tft.height()-1, color);
-    }
-    for (uint16_t y=0; y < tft.height()-1; y+=6) {
-        tft.drawLine(tft.width()-1, 0, 0, y, color);
-    }
-
-    tft.fillScreen(BLACK);
-    for (uint16_t x=0; x < tft.width()-1; x+=6) {
-        tft.drawLine(0, tft.height()-1, x, 0, color);
-    }
-    for (uint16_t y=0; y < tft.height()-1; y+=6) {
-        tft.drawLine(0, tft.height()-1, tft.width()-1, y, color);
-    }
-
-    tft.fillScreen(BLACK);
-    for (uint16_t x=0; x < tft.width()-1; x+=6) {
-        tft.drawLine(tft.width()-1, tft.height()-1, x, 0, color);
-    }
-    for (uint16_t y=0; y < tft.height()-1; y+=6) {
-        tft.drawLine(tft.width()-1, tft.height()-1, 0, y, color);
-    }
-
-}
-
 void testdrawtext(char *text, uint16_t color) {
     tft.setCursor(0,0);
     tft.setTextColor(color);
     tft.print(text);
 }
 
-void testfastlines(uint16_t color1, uint16_t color2) {
-    tft.fillScreen(BLACK);
-    for (uint16_t y=0; y < tft.height()-1; y+=5) {
-        tft.drawFastHLine(0, y, tft.width()-1, color1);
-    }
-    for (uint16_t x=0; x < tft.width()-1; x+=5) {
-        tft.drawFastVLine(x, 0, tft.height()-1, color2);
-    }
-}
-
-void testdrawrects(uint16_t color) {
-    tft.fillScreen(BLACK);
-    for (uint16_t x=0; x < tft.height()-1; x+=6) {
-        tft.drawRect((tft.width()-1)/2 -x/2, (tft.height()-1)/2 -x/2 , x, x, color);
-    }
-}
-
-void testfillrects(uint16_t color1, uint16_t color2) {
-    tft.fillScreen(BLACK);
-    for (uint16_t x=tft.height()-1; x > 6; x-=6) {
-        tft.fillRect((tft.width()-1)/2 -x/2, (tft.height()-1)/2 -x/2 , x, x, color1);
-        tft.drawRect((tft.width()-1)/2 -x/2, (tft.height()-1)/2 -x/2 , x, x, color2);
-    }
-}
-
-void testfillcircles(uint8_t radius, uint16_t color) {
-    for (uint8_t x=radius; x < tft.width()-1; x+=radius*2) {
-        for (uint8_t y=radius; y < tft.height()-1; y+=radius*2) {
-            tft.fillCircle(x, y, radius, color);
-        }
-    }
-}
-
-void testdrawcircles(uint8_t radius, uint16_t color) {
-    for (uint8_t x=0; x < tft.width()-1+radius; x+=radius*2) {
-        for (uint8_t y=0; y < tft.height()-1+radius; y+=radius*2) {
-            tft.drawCircle(x, y, radius, color);
-        }
-    }
-}
-
-void testtriangles() {
-    tft.fillScreen(BLACK);
-    int color = 0xF800;
-    int t;
-    int w = tft.width()/2;
-    int x = tft.height();
-    int y = 0;
-    int z = tft.width();
-    for(t = 0 ; t <= 15; t+=1) {
-        tft.drawTriangle(w, y, y, x, z, x, color);
-        x-=4;
-        y+=4;
-        z-=4;
-        color+=100;
-    }
-}
-
-void testroundrects() {
-    tft.fillScreen(BLACK);
-    int color = 100;
-
-    int x = 0;
-    int y = 0;
-    int w = tft.width();
-    int h = tft.height();
-    for(int i = 0 ; i <= 24; i++) {
-        tft.drawRoundRect(x, y, w, h, 5, color);
-        x+=2;
-        y+=3;
-        w-=4;
-        h-=6;
-        color+=1100;
-        Serial.println(i);
-    }
-}
-
-void tftPrintTest() {
-    tft.fillScreen(BLACK);
-    tft.setCursor(0, 5);
-    tft.setTextColor(RED);
-    tft.setTextSize(1);
-    tft.println("Hello World!");
-    tft.setTextColor(YELLOW);
-    tft.setTextSize(2);
-    tft.println("Hello World!");
-    tft.setTextColor(BLUE);
-    tft.setTextSize(3);
-    tft.print(1234.567);
-    delay(1500);
-    tft.setCursor(0, 5);
-    tft.fillScreen(BLACK);
-    tft.setTextColor(WHITE);
-    tft.setTextSize(0);
-    tft.println("Hello World!");
-    tft.setTextSize(1);
-    tft.setTextColor(GREEN);
-    tft.print(p, 6);
-    tft.println(" Want pi?");
-    tft.println(" ");
-    tft.print(8675309, HEX); // print 8,675,309 out in HEX!
-    tft.println(" Print HEX!");
-    tft.println(" ");
-    tft.setTextColor(WHITE);
-    tft.println("Sketch has been");
-    tft.println("running for: ");
-    tft.setTextColor(MAGENTA);
-    tft.print(millis() / 1000);
-    tft.setTextColor(WHITE);
-    tft.print(" seconds.");
-}
-
-void mediabuttons() {
-    // play
-    tft.fillScreen(BLACK);
-    tft.fillRoundRect(25, 10, 78, 60, 8, WHITE);
-    tft.fillTriangle(42, 20, 42, 60, 90, 40, RED);
-    delay(500);
-    // pause
-    tft.fillRoundRect(25, 90, 78, 60, 8, WHITE);
-    tft.fillRoundRect(39, 98, 20, 45, 5, GREEN);
-    tft.fillRoundRect(69, 98, 20, 45, 5, GREEN);
-    delay(500);
-    // play color
-    tft.fillTriangle(42, 20, 42, 60, 90, 40, BLUE);
-    delay(50);
-    // pause color
-    tft.fillRoundRect(39, 98, 20, 45, 5, RED);
-    tft.fillRoundRect(69, 98, 20, 45, 5, RED);
-    // play color
-    tft.fillTriangle(42, 20, 42, 60, 90, 40, GREEN);
-}
-
-/**************************************************************************/
-/*!
-    @brief  Renders a simple test pattern on the screen
-*/
-/**************************************************************************/
-void lcdTestPattern(void)
-{
-    static const uint16_t PROGMEM colors[] =
-            { RED, YELLOW, GREEN, CYAN, BLUE, MAGENTA, BLACK, WHITE };
-
-    for(uint8_t c=0; c<8; c++) {
-        tft.fillRect(0, tft.height() * c / 8, tft.width(), tft.height() / 8,
-                     pgm_read_word(&colors[c]));
-    }
+char line = 0;
+void printLn(char *text) {
+    tft.setCursor(0, line);
+    tft.print(text);
+    line += 12;
 }
 
 void setup(void) {
@@ -281,82 +101,33 @@ void setup(void) {
     time = millis() - time;
 
     Serial.println(time, DEC);
-    delay(500);
 
-    lcdTestPattern();
-    delay(500);
+    tft.setTextColor(GREEN);
+    printLn("Artificial Horizon v0.9");
 
-    if (RUN_TEST_PATTERNS != 0) {
-        tft.invert(true);
-        delay(100);
-        tft.invert(false);
-        delay(100);
-
-        tft.fillScreen(BLACK);
-        testdrawtext(
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur adipiscing ante sed nibh tincidunt feugiat. Maecenas enim massa, fringilla sed malesuada et, malesuada sit amet turpis. Sed porttitor neque ut ante pretium vitae malesuada nunc bibendum. Nullam aliquet ultrices massa eu hendrerit. Ut sed nisi lorem. In vestibulum purus a tortor imperdiet posuere. ",
-                WHITE);
-        delay(500);
-
-        // tft print function!
-        tftPrintTest();
-        delay(500);
-
-        //a single pixel
-        tft.drawPixel(tft.width() / 2, tft.height() / 2, GREEN);
-        delay(500);
-
-        // line draw test
-        testlines(YELLOW);
-        delay(500);
-
-        // optimized lines
-        testfastlines(RED, BLUE);
-        delay(500);
-
-
-        testdrawrects(GREEN);
-        delay(1000);
-
-        testfillrects(YELLOW, MAGENTA);
-        delay(1000);
-
-        tft.fillScreen(BLACK);
-        testfillcircles(10, BLUE);
-        testdrawcircles(10, WHITE);
-        delay(1000);
-
-        testroundrects();
-        delay(500);
-
-        testtriangles();
-        delay(500);
-    }
-
-    Serial.println("done");
-    delay(1000);
-
-    tft.fillScreen(BLACK);
     if (mpu.begin()) {
         hasMpu = 1;
         mpu.setAccelerometerRange(MPU6050_RANGE_16_G);
         mpu.setGyroRange(MPU6050_RANGE_250_DEG);
         mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
+        printLn("MPU initialised.");
     } else {
         testdrawtext("Cannot initialise MPU", RED);
     }
+    delay(500);
+     tft.fillScreen(BLACK);
 }
 
 float v[7] = { 0, 0, 0, 0, 0, 0, 0 };
-char first = 1;
+//char first = 1;
 
 void printValue(float value, int index) {
     int y = index * 12;
-    if (!first) {
-        tft.setTextColor(BLACK);
-        tft.setCursor(0, y);
-        tft.print(v[index]);
-    }
+//    if (!first) {
+//        tft.setTextColor(BLACK);
+//        tft.setCursor(0, y);
+//        tft.print(v[index]);
+//    }
     tft.setTextColor(index < 3 ? WHITE : index < 6 ? YELLOW : CYAN);
     tft.setCursor(0, y);
     tft.print(value);
@@ -367,21 +138,224 @@ void testMpu6050() {
     sensors_event_t a, g, temp;
     mpu.getEvent(&a, &g, &temp);
 
-    printValue(a.acceleration.x, 0);
-    printValue(a.acceleration.y, 1);
-    printValue(a.acceleration.z, 2);
+//    printValue(a.acceleration.x, 0);
+//    printValue(a.acceleration.y, 1);
+//    printValue(a.acceleration.z, 2);
+//
+//    printValue(g.gyro.x, 3);
+//    printValue(g.gyro.y, 4);
+//    printValue(g.gyro.z, 5);
+//
+//    printValue(temp.temperature, 6);
+//    first = 0;
+}
 
-    printValue(g.gyro.x, 3);
-    printValue(g.gyro.y, 4);
-    printValue(g.gyro.z, 5);
+// We want to display numbers, and we want to do that very fast.
+// Rather than building a RAM buffer - we don't have much RAM -
+// we can hardcode our digit renderers. And rather than overlaying
+// it, and double-rendering and introducing flicker, it's far quicker
+// to do all rendering in one pass. Without memory for a frame buffer,
+// we therefore need to do this in our hardcoded write function.
+// Either that, or we use 10% of the chip's memory as a row or column
+// buffer, and write only to that.
+char characters[6*7] = {};
+const char char0 = 0;
+const char char1 = 1;
+const char char2 = 2;
+const char char3 = 3;
+const char char4 = 4;
+const char char5 = 5;
+const char char6 = 6;
+const char char7 = 7;
+const char char8 = 8;
+const char char9 = 9;
+const char charNeg = 10;
+const char charDot = 11;
+const char charSpace = 15;
 
-    printValue(temp.temperature, 6);
-    first = 0;
+void printToBuffer(char row, char colourIndex, float value) {
+    char colourBits = colourIndex << 4;
+    char offset = row * 7;
+    int16_t ivalue = value * 100; // work with ints
+    characters[offset + 0] = colourBits | (ivalue < 0 ? charNeg : charSpace);
+    if (ivalue < 0) {
+        ivalue = -ivalue;
+    }
+    if (ivalue > 9999) {
+        ivalue = ivalue % 10000;
+    }
+    characters[offset + 5] = colourBits | (ivalue % 10);
+    ivalue /= 10;
+    characters[offset + 4] = colourBits | (ivalue % 10);
+    ivalue /= 10;
+    characters[offset + 3] = charDot;
+    characters[offset + 2] = colourBits | (ivalue == 0 ? charSpace : ivalue % 10);
+    ivalue /= 10;
+    characters[offset + 1] = colourBits | (ivalue == 0 ? charSpace : ivalue % 10);
+}
+
+// 16bit colours are 5 bits red, 6 bits green, 5 bits blue
+// 116, 203, 255
+const uint16_t blue = 0x733F; // 01110 011001 11111
+// 179, 121, 41
+const uint16_t brown = 0x63C5; // 01100 011110 00101
+
+char overlayBuffer[16] = {};
+char brownBuffer[16] = {};
+
+// This is horribly slow
+void drawHorizonVLineRunLength(int16_t x, int16_t yintercept) {
+    // create Horizon layer
+    uint8_t y = 0;
+    if (yintercept < 0) {
+        for (int i = 0; i < 16; i++) {
+            brownBuffer[i] = 0xFF;
+        }
+    } else if (yintercept > 127) {
+        for (int i = 0; i < 16; i++) {
+            brownBuffer[i] = 0x00;
+        }
+    } else {
+        // Which block should we bash bits in?
+        // yintercept   block   bit_start   bits
+        // 0            0       1           FF
+        // 1            0       2           FE
+        // 2            0       4           FC
+        // 7            0       128         1
+        // 8            1       0           FF
+        // 9            1       1           FE
+        const uint8_t yintercept_block = yintercept >> 3;
+        y = 0;
+        while (y < yintercept_block) {
+            brownBuffer[y++] = 0;
+        }
+        // FF >> 0 == FF, FF >> 7 ==
+        const uint8_t shift = yintercept & 0x07;
+        brownBuffer[y++] = 0xFF << shift;
+        while (y < 16) {
+            brownBuffer[y++] = 0xFF;
+        }
+    }
+
+    // Create character layer
+    if (x < 32) {
+        y = 0;
+        while (y < 4) {
+            overlayBuffer[y++] = 0x11;
+        }
+        while (y < 16) {
+            overlayBuffer[y++] = 0x00;
+        }
+    }
+
+    // Combine layers into a set of pixel operations
+    uint16_t runColour = blue;
+    int16_t runStart = 0;
+    uint8_t runLength = 0;
+    for(y = 0; y < 128; y++) {
+        const uint8_t block = y >> 3;
+        const uint8_t bit = 1 << (y & 0x07);
+        uint16_t c = blue;
+        if (brownBuffer[block] & bit) {
+            c = brown;
+        }
+        if (overlayBuffer[block] & bit) {
+            c = WHITE;
+        }
+        if (c == runColour) {
+            runLength++;
+        } else {
+            // Output the last run
+            if (runLength > 0) {
+                tft.writeFillRectPreclipped(x, runStart, 1, runLength, runColour);
+                //tft.drawFastVLine(x, runStart, runLength, runColour);
+            }
+            // Start a new one.
+            runColour = c;
+            runStart = y;
+            runLength = 1;
+        }
+    }
+    // Output the last run.
+    tft.writeFillRectPreclipped(x, runStart, 1, runLength, runColour);
+    //tft.drawFastVLine(x, runStart, runLength, runColour);
+}
+
+uint16_t colourBuffer[16] = {};
+
+// This is relatively fast.
+void drawHorizonVWritePixels(int16_t x, int16_t yintercept) {
+    for (int band = 0; band < 8; band++) {
+        int ystart = band * 16;
+        int yend = ystart + 16;
+        tft.setAddrWindow(x, ystart, 1, 16);
+        int y = 0;
+        while (y < yintercept && y < 16) {
+            colourBuffer[y++] = blue;
+        }
+        while (y >= yintercept && y < 16) {
+            colourBuffer[y++] = brown;
+        }
+        yintercept -= 16;
+        // Overlay "text"
+        if (band == 0) {
+            colourBuffer[0] = WHITE;
+            colourBuffer[4] = WHITE;
+            colourBuffer[8] = WHITE;
+            colourBuffer[12] = WHITE;
+        }
+        tft.writePixels(colourBuffer, 16, true, false);
+    }
+}
+
+int16_t test = 0;
+
+void drawHorizon() {
+    sensors_event_t a, g, temp;
+    mpu.getEvent(&a, &g, &temp);
+    printToBuffer(0, 0, a.acceleration.x);
+    printToBuffer(1, 0, a.acceleration.y);
+    printToBuffer(2, 0, a.acceleration.z);
+//    printValue(a.acceleration.x, 0);
+//    printValue(a.acceleration.y, 1);
+//    printValue(a.acceleration.z, 2);
+
+    // Transaction
+    tft.startWrite();
+
+    // Find the equation of a line which expresses the horizon.
+    // Using slope-intercept y = mx + c. Note that we will need
+    // special handling to cope with 90 degrees, since m will be INF
+    float m = 0;
+    float c = 0 + test % 128 - 64;
+    int16_t y = 0;
+    test ++;
+
+
+    // Draw the horizon - brown below the line, blue above.
+    for (int16_t x = 0; x < 128; x++) {
+        y = m * x + c + 64;
+        // Draw 0-y as blue
+//        drawHorizonVLineRunLength(x, y);
+        drawHorizonVWritePixels(x, y);
+
+//        if (y > 0) {
+//            tft.writeFillRectPreclipped(x, 0, 1, y, blue);
+////            tft.writeFastVLine(x, 0, y, blue);
+//        }
+//        if (y < 128) {
+//            tft.writeFillRectPreclipped(x, y, 1, 127-y, brown);
+////            tft.writeFastVLine(x, y, 127-y, brown);
+//        }
+    }
+
+    tft.endWrite();
 }
 
 void loop() {
     if (hasMpu == 1) {
-        testMpu6050();
-        delay(100);
+        drawHorizon();
+        //testMpu6050();
+        //delay(100);
     }
 }
